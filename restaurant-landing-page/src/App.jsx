13f942562c657fcdf978bebe2436d66c1d9c7484
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-09-23 21:47:02 -0300

    First commit

---
+++ restaurant-landing-page/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1 className="text-red-500 text-center ">Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
