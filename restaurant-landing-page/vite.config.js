a7deb7d3544907f96b74da09c75ad25f273b7930
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-09-26 19:48:06 -0300

    update vite.config,js

--- restaurant-landing-page/vite.config.js
+++ restaurant-landing-page/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 
   plugins: [react()],
-  base:"/Foodie/"
+  base:"https://cecimartinez.github.io/restaurant-foodie"
 
 })
